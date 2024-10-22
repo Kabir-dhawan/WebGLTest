1b7d7202fa33b4ed99e97c9ee666a80dddfb7d04
gamedevteam321 <gamedevteam@greyblue.in>
2024-10-22 18:37:06 +0530

    updated avatar camera

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,12 @@ import { useSearchParams } from 'react-router-dom';
 import { XR, createXRStore } from '@react-three/xr'
 
 const store = createXRStore();
-
+const config = {
+    clearCache: false,
+    bodyType: 'fullbody',
+    quickStart: false,
+    language: 'en',
+  };
 export default function Experience(){
     //const [avatarUrl, setAvatarUrl] = useState('https://models.readyplayer.me/670e069cb8d37556eaee135d.glb');
     const [avatarUrl, setAvatarUrl] = useState('');
@@ -18,25 +23,48 @@ export default function Experience(){
       console.log("testing next",url);
       setAvatarUrl(url);
     };
+
+    const handleOnUserSet = (event) => {
+        console.log(`User ID is: ${event.data.id}`);
+      };
+    
+      const handleOnAvatarExported = (event) => {
+        console.log(`Avatar URL is: ${event.data.url}`);
+      };
+    
+      const handleUserAuthorized = (event) => {
+        console.log(`User is:`, event.data);
+      };
+    
+      const handleAssetUnlocked = (event) => {
+        console.log(`Asset unlocked is: ${event.data.assetId}`);
+      };
+
 return (<>
     <div className="fixed top-0 left-0 z-10 w-screen h-screen">
         {/* Show the avatar creator if no avatar URL is set */}
         {!avatarUrl && (
             <AvatarCreator
-            subdomain="ytgvbhjn"
+            //subdomain="ytgvbhjn"
+            subdomain="gametest-3j77ud"
+            //subdomain="demo"
+            config={config} 
             className="fixed top-0 left-0 z-10 w-screen h-screen"
             onAvatarExported={(event) => {
                 console.log("Avatar creation testing");
                 handleAvatarCreated( event.data.url);
             }}
-            onClose={() => { /* Handle closing the avatar creator (optional) */ }}
+            onUserAuthorized={handleUserAuthorized}
+            onAssetUnlock={handleAssetUnlocked}
+            onUserSet={handleOnUserSet}
             />
         )}
 
         {/* Show the avatar if an avatar URL is available */}
         {avatarUrl && (
             <>
-            <button onClick={() => store.enterAR()}>Enter AR</button>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={()=> setAvatarUrl('')}> Change Avatar</button>
+            {/* <button onClick={() => store.enterAR()}>Enter AR</button> */}
             <Canvas className="fixed top-0 left-0 z-10 w-screen h-screen" >
             <XR store={store}>
                 <Suspense>
